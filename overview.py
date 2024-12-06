1be6105521ce20795d323b84ee52d7dac40dd1c3
tranhungafd <126379430+tranhungafd@users.noreply.github.com>
2024-12-06 11:03:26 +0700

    Update overview.py

--- overview.py
+++ overview.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from vnstock3 import Vnstock
-from datetime import datetime
+from datetime import datetime, timedelta
 import plotly.graph_objects as go
+import plotly.express as px
 import pandas as pd
+from plotly.subplots import make_subplots
+
 st.set_page_config(layout="wide")
 now =  datetime.now().strftime('%Y-%m-%d')
 st.title('Phân Tích Cổ phiếu')
@@ -40,3 +43,35 @@ st.markdown(f"""
 st.markdown(f"""
     <h4 style='text-align: center;'>{name}</h4>
     """, unsafe_allow_html=True)
+company = Vnstock().stock(symbol=selected_stock, source='TCBS').company
+df = company.shareholders()
+
+col1,spacer2, col2 = st.columns([3,0.5,2])
+with col1:
+    st.markdown("### **Cơ cấu sở hữu của cổ đông**")
+    df['Tỷ lệ'] = df['share_own_percent'] * 100
+    total_excluding_other = df[df['share_holder'] != 'Khác']['Tỷ lệ'].sum()
+    df.loc[df['share_holder'] == 'Khác', 'Tỷ lệ'] = 100 - total_excluding_other
+    df['Tên cổ đông'] = df['share_holder']
+    fig = px.pie(
+        df, 
+        values='Tỷ lệ', 
+        names='Tên cổ đông', 
+        color_discrete_sequence=px.colors.qualitative.Set3,
+    )
+    st.plotly_chart(fig, use_container_width=True)
+with col2:
+    stock = Vnstock().stock(symbol=selected_stock, source='VCI')
+    df = stock.finance.ratio(period='year', lang='vi', dropna=True)
+    row = df['Chỉ tiêu định giá'].iloc[0]
+
+    # Hiển thị thông tin
+    st.markdown("### **Thông tin định giá**")
+    st.write(f"**Vốn hóa (Tỷ đồng):** {round(row['Vốn hóa (Tỷ đồng)'],2):,}")
+    st.write(f"**Số CP lưu hành (Triệu CP):** {row['Số CP lưu hành (Triệu CP)']:,}")
+    st.write(f"**P/E:** {round(row['P/E'],2)}")
+    st.write(f"**P/B:** {round(row['P/B'],2)}")
+    st.write(f"**P/S:** {round(row['P/S'],2)}")
+    st.write(f"**P/Cash Flow:** {round(row['P/E'],2)}")
+    st.write(f"**EPS (VND):** {round(row['EPS (VND)'],2):,}")
+    st.write(f"**BVPS (VND):** {round(row['BVPS (VND)'],2):,}")
